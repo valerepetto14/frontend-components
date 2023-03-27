64899f938921ff459502f9097d04778ea07c9a64
Valentin Repetto <valentinrepetto12345@gmail.com>
2023-03-27 12:10:44 -0300

    add flipCard

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import FlipCard from './components/FlipCard'
+import kingman from './assets/kingsman.jpg'
+import Spider from './assets/spider.jpg'
+import RedNotice from './assets/rednotice.jpg'
+
+function App() {
+  return (
+    <div className="bg-gray-900 App w-full h-screen flex justify-evenly items-center">
+      <FlipCard 
+        img={kingman}
+        txt={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum doloremque blanditiis modi quibusdam perferendis ducimus distinctio? Magnam, perferendis sint quod accusamus expedita odio cumque quibusdam ipsam, consequuntur esse culpa rem."} />
+      <FlipCard 
+        img={Spider} 
+        txt={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum doloremque blanditiis modi quibusdam perferendis ducimus distinctio? Magnam, perferendis sint quod accusamus expedita odio cumque quibusdam ipsam, consequuntur esse culpa rem."}
+        />
+      <FlipCard 
+        img={RedNotice}
+        txt={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum doloremque blanditiis modi quibusdam perferendis ducimus distinctio? Magnam, perferendis sint quod accusamus expedita odio cumque quibusdam ipsam, consequuntur esse culpa rem."}
+        />
+    </div>
+  )
+}
+
+export default App
